6b3b2435d17b7bdece7e9855830526fe04f26aca
MichaelZolotov77 <michaelzol@yandex.ru>
2024-03-12 15:56:55 +0300

    Хук useHistory

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,10 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 
 function Movie() {
   const { title } = useParams();
+  const { goBack } = useHistory();
   //   console.log(value);
 
-  return <h1>Some move {title}</h1>;
+  return (
+    <>
+      <h1>Some move {title}</h1>
+      <button className="btn" onClick={goBack}>
+        Go back{" "}
+      </button>
+    </>
+  );
 }
 
 export { Movie };
