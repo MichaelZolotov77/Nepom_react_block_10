7f6ee9d7ee63921e40561eb35b264b9c5e4e0af3
MichaelZolotov77 <michaelzol@yandex.ru>
2024-03-16 13:26:36 +0300

    Страница категории

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Contact } from "./pages/Contact";
 import { NotFound } from "./pages/NotFound";
-import { Movie } from "./pages/Movie";
+
+import { Category } from "./pages/Category";
 
 function App() {
   return (
@@ -21,7 +22,7 @@ function App() {
             </Route>
             <Route path="/about" component={About} />
             <Route path="/contacts" component={Contact} />
-            <Route path="/movies/:title" component={Movie} />
+            <Route path="/category/:name" component={Category} />
             <Route component={NotFound} />
           </Switch>
         </main>
